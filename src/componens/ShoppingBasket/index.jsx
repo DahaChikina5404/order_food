ae5481623e02ca80dbef08d09ef9bcbea8277c61
DahaChikina5404 <valentina_kpss@mail.ru>
2024-02-18 22:27:35 +0700

    Настроена ссылка на домашнюю страницу

--- src/componens/ShoppingBasket/index.jsx
+++ src/componens/ShoppingBasket/index.jsx
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom"
 function ShoppingBasket() {
 
     const [cartItems, setCartItems] = useState([
-        id,
-        image,
-        name,
-        price,
-        count
+        // id,
+        // image,
+        // name,
+        // price,
+        // count
     ])
 
 
-    const deleteOrder = (id) => {
-        const filteredOrder = cartItems.filter(cartItem => cartItem.id !== id)
-        setCartItems(filteredOrder)
-    }
+    // const deleteOrder = (id) => {
+    //     const filteredOrder = cartItems.filter(cartItem => cartItem.id !== id)
+    //     setCartItems(filteredOrder)
+    // }
 
 
 
@@ -26,11 +26,13 @@ function ShoppingBasket() {
                 <div className="mt-20 text-center text-3xl md:text-6xl text-gray-400 font-thin">
                     <p>Ваша корзина пуста!</p>
                     <Link to='/'>
-                        <p className="mt-7 text-red-200 hover:text-gray-400">Перейдите для заказа</p>
+                        <p className="my-7 text-red-200 hover:text-gray-400">Перейдите для заказа</p>
                     </Link>
                 </div>    
             ) : (
                 <div>
+                    <h2 className="my-10 text-center text-3xl">Ваш заказ:</h2>
+
                 </div>
 
 
